63280ea6515d5b645c443470ea22742e0458bd6e
shuvam021 <career.shuvam@gmail.com>
2022-03-24 01:17:22 +0530

    [Shuvam] Refactor Note view for retieve cached data instade of database

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 import logging
 
+from django.core.cache import cache
+from django.forms.models import model_to_dict
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, views
 from rest_framework.permissions import AllowAny, IsAdminUser
@@ -110,6 +112,70 @@ class NoteViewSet(viewsets.ModelViewSet):
         user = self.request.user
         queryset = Note.objects.filter(user=user)
         return queryset
+
+    def update_cache(self, user_id):
+        """Update cached memory"""
+        json_data = [{item.id: model_to_dict(item)} for item in self.get_queryset()]
+        cache.set(user_id, json_data)
+
+    def list(self, request, *args, **kwargs):
+        try:
+            dataset = cache.get(request.user.id)
+            if dataset:
+                data = [v for item in dataset for k, v in item.items()]
+                return Response(data=data)
+            return Response(data=[])
+        except Exception as e:
+            logger.exception(e)
+            return Response(status=400)
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            pk = kwargs.get('pk')
+            cached_data = list(cache.get(request.user.id))
+            if cached_data:
+                dataset = list(filter(None, [i.get(int(pk)) for i in cached_data]))
+                data = {} if len(dataset) == 0 else dataset[0]
+                return Response(data=data, status=200)
+            return Response(data={})
+        except Exception as e:
+            logger.exception(e)
+            return Response(status=400)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            self.update_cache(request.user.id)
+            return Response(data=serializer.data)
+        except Exception as e:
+            logger.exception(e)
+            return Response(data=serializer.errors)
+
+    def update(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        note = get_object_or_404(Note, pk=pk)
+        serializer = self.get_serializer(note, data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            self.update_cache(request.user.id)
+            return Response(data=serializer.data)
+        except Exception as e:
+            logger.exception(e)
+            return Response(data=serializer.errors)
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            pk = kwargs.get('pk')
+            note = get_object_or_404(Note, pk=pk)
+            note.delete()
+            self.update_cache(request.user.id)
+            return Response(status=204)
+        except Exception as e:
+            logger.exception(e)
+            return Response({'message': str(e)})
 
 
 class LabelViewSet(viewsets.ModelViewSet):
